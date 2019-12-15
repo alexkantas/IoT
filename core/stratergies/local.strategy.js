d2705d1d6265b5d058dfba51a0da7cd7b2112f3a
alexkantas <alexininternet@gmail.com>
2019-12-15 23:08:59 +0200

    REMOVE need for database

--- core/stratergies/local.strategy.js
+++ core/stratergies/local.strategy.js
@@ -3,33 +3,18 @@ const LocalStategy = require('passport-local').Strategy;
 const User = require("../../models/user.model.js");
 const mongoose = require('mongoose');
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/thesis';
-const mongoConnect = mongoose.connect(mongoUrl, { useMongoClient: true, useUnifiedTopology: true });
+// const mongoConnect = mongoose.connect(mongoUrl, { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
+const usernameList = ['ALEX', 'LEFTERIS', 'GEORGE', 'MARIA', 'ANNA']
 
 const userStategy = function () {
     passport.use(
         new LocalStategy({ usernameField: 'username', passwordField: 'password' },
             (username, password, done) => {
-                console.log('Mesa sth strategi!!');
-                User.findOne({ username, password })
-                    .then(user => {
-                        console.log('Brhkame ton user!!');
-                        if (user) {
-                            done(null, user);
-                            return;
-                        }
-                        done(null, false, { message: 'Wrong creadantials' });
-                    })
-                    .catch(err => {
-                        console.log('Error' + err)
-                        done(null, {
-                            "username": "GUEST".concat(Date.now()),
-                            "password": "GUEST",
-                        });
-                    });
-            }
-        ));
+                if (usernameList.includes(username)) return done(null, { username });
+                done(null, {username: 'GUEST'})
+            }));
 }
 
 module.exports = userStategy;
